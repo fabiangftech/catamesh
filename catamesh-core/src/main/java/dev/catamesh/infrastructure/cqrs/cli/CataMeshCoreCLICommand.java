6bb7685593f5f4a45f1e63ba439c099da088a77e
fabiangftech <fabian.gf.tech@gmail.com>
2026-03-13 14:20:17 -0300

    refactor: refactor cli's 1

--- catamesh-core/src/main/java/dev/catamesh/infrastructure/cqrs/cli/CataMeshCoreCLICommand.java
+++ catamesh-core/src/main/java/dev/catamesh/infrastructure/cqrs/cli/CataMeshCoreCLICommand.java
@@ -1,93 +1,30 @@
 package dev.catamesh.infrastructure.cqrs.cli;
 
-import dev.catamesh.core.model.ModelType;
-import dev.catamesh.infrastructure.adapter.CLIErrorAdapter;
-import dev.catamesh.infrastructure.adapter.CLIJsonAdapter;
-import dev.catamesh.infrastructure.dto.CLIErrorPayloadDTO;
-import tools.jackson.databind.ObjectMapper;
+import java.util.stream.IntStream;
 
-import java.util.Map;
-import java.util.function.Consumer;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
-@SuppressWarnings("java:S106")
 public class CataMeshCoreCLICommand {
-    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-    private static final Map<String, Consumer<String[]>> COMMAND_HANDLERS = Map.of(
-            "new", NewTemplateCLICommand::main,
-            "plan", CataMeshCoreCLICommand::runDataProductCommand,
-            "apply", CataMeshCoreCLICommand::runDataProductCommand,
-            "get", CataMeshCoreCLICommand::get,
-            "diff", CataMeshCoreCLICommand::runDataProductCommand,
-            "destroy", CataMeshCoreCLICommand::destroy
-    );
+    public static final String DIFF = "diff";
+    public static final String PLAN = "plan";
+    public static final String APPLY = "apply";
+    public static final String GET = "get";
 
     public static void main(String[] command) {
-        int status = execute(command);
-        if (status != 0) {
-            System.exit(status);
-        }
-    }
-
-    public static int execute(String[] command) {
-        configureCliLogging();
+        String[] aux =
+                IntStream.range(0, command.length).filter(i -> i != 1)
+                        .mapToObj(i -> command[i])
+                        .toArray(String[]::new);
         try {
-            dispatch(command[0], command);
-            return 0;
-        } catch (Throwable error) {
-            CLIErrorPayloadDTO payload = CLIErrorAdapter.map(error);
-            writeError(payload);
-            return payload.getStatus();
-        }
-    }
-
-    private static void dispatch(String verb, String[] command) {
-        Consumer<String[]> handler = COMMAND_HANDLERS.get(verb);
-        if (handler == null) {
-            return;
-        }
-        handler.accept(command);
-    }
-
-    private static void runDataProductCommand(String[] command) {
-        //todo check if schema is data-product/v1
-        DataProductCLICommand.main(command);
-    }
-
-    private static void get(String[] command) {
-        String type = command[1]; // data-product/deploy/env
-        if (!ModelType.DATA_PRODUCT.getValue().equals(type)) {
-            return;
-        }
-        DataProductCLICommand.main(command);
-    }
-
-    private static void destroy(String[] command) {
-        System.out.println("Not implemented destroy yet!");
-    }
-
-    private static void configureCliLogging() {
-        Logger rootLogger = LogManager.getLogManager().getLogger("");
-        if (rootLogger == null) {
-            return;
-        }
-
-        rootLogger.setLevel(Level.OFF);
-        for (Handler handler : rootLogger.getHandlers()) {
-            handler.setLevel(Level.OFF);
-        }
-    }
-
-    private static void writeError(CLIErrorPayloadDTO payload) {
-        try {
-            System.err.println(CLIJsonAdapter.toJson(payload, JSON_MAPPER));
-        } catch (Exception serializationError) {
-            System.err.println(
-                    "{\"errorCode\":\"INTERNAL_ERROR\",\"status\":25,\"title\":\"Internal error\",\"message\":\"Failed to serialize CLI error payload.\"}"
-            );
+            switch (command[0]) {
+                case DIFF, PLAN, APPLY, GET -> {
+                    if (command[1].equals("data-product")) {
+                        DataProductCLICommand.main(aux);
+                    }
+                }
+                default -> {
+                }
+            }
+        } catch (Exception e) {
+            //todo check exception
         }
     }
 }
--- catamesh-core/src/main/java/dev/catamesh/infrastructure/cqrs/cli/DataProductCLICommand.java
+++ catamesh-core/src/main/java/dev/catamesh/infrastructure/cqrs/cli/DataProductCLICommand.java
@@ -5,35 +5,37 @@ import dev.catamesh.core.model.DiffResult;
 import dev.catamesh.core.model.PlanResult;
 import dev.catamesh.infrastructure.config.AppConfig;
 
+import static dev.catamesh.infrastructure.cqrs.cli.CataMeshCoreCLICommand.*;
+
 @SuppressWarnings("java:S106")
 public class DataProductCLICommand {
     private static final AppConfig appConfig = new AppConfig();
 
     public static void main(String[] args) {
-        String verb = args[0];  // plan - apply - get - diff
+        String verb = args[0];
         String yaml;
         String dataProductName;
         String json;
         switch (verb) {
-            case "diff":
+            case DIFF:
                 yaml = args[1];
                 DiffResult diffResult = appConfig.dataProductFacade().diff(yaml);
                 json = appConfig.jsonMapper().writeValueAsString(diffResult);
                 System.out.println(json);
                 break;
-            case "plan":
+            case PLAN:
                 yaml = args[1];
                 PlanResult   planResult = appConfig.dataProductFacade().plan(yaml);
                 json = appConfig.jsonMapper().writeValueAsString(planResult);
                 System.out.println(json);
                 break;
-            case "apply":
+            case APPLY:
                 yaml = args[1];
                 ApplyResult applyResult = appConfig.dataProductFacade().apply(yaml);
                 json = appConfig.jsonMapper().writeValueAsString(applyResult);
                 System.out.println(json);
                 break;
-            case "get":
+            case GET:
                 dataProductName = args[2];
                 //DataProduct dataProduct = dataProductFacade.get(dataProductName);
                 //System.out.println(CLIJsonAdapter.toJson(dataProduct, jsonMapper));
