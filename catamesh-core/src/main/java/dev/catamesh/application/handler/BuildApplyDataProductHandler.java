dc595abe900cddc36171543180d7385f183c9c73
fabiangftech <fabian.gf.tech@gmail.com>
2026-03-14 10:55:51 -0300

    chore: base for definition's create 3

--- catamesh-core/src/main/java/dev/catamesh/application/handler/BuildApplyDataProductHandler.java
+++ catamesh-core/src/main/java/dev/catamesh/application/handler/BuildApplyDataProductHandler.java
@@ -4,12 +4,16 @@ import dev.catamesh.core.handler.ApplyDataProductContext;
 import dev.catamesh.core.handler.Handler;
 import dev.catamesh.core.model.ApplyResult;
 
+import java.time.LocalDateTime;
+
 public class BuildApplyDataProductHandler<Context> extends Handler<Context> {
 
     @Override
     protected void doHandle(Context context) {
         ApplyDataProductContext applyDataProductContext = (ApplyDataProductContext) context;
         ApplyResult applyResult = new ApplyResult();
+        applyResult.setDataProductName(applyDataProductContext.getDesiredDataProduct().getMetadata().getName());
+        applyResult.setExecutedAt(LocalDateTime.now());
         applyDataProductContext.setApplyResult(applyResult);
     }
 }
--- catamesh-core/src/main/java/dev/catamesh/core/model/ApplyResult.java
+++ catamesh-core/src/main/java/dev/catamesh/core/model/ApplyResult.java
@@ -3,6 +3,7 @@ package dev.catamesh.core.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -11,9 +12,25 @@ public class ApplyResult {
     private ApplyStatus status;
     private List<ApplyStep> steps;
     private ApplySummary summary;
-    private Instant executedAt;
+    private LocalDateTime executedAt;
 
     public ApplyResult() {
         // do nothing
+    }
+
+    public String getDataProductName() {
+        return dataProductName;
+    }
+
+    public LocalDateTime getExecutedAt() {
+        return executedAt;
+    }
+
+    public void setExecutedAt(LocalDateTime executedAt) {
+        this.executedAt = executedAt;
+    }
+
+    public void setDataProductName(String dataProductName) {
+        this.dataProductName = dataProductName;
     }
 }
