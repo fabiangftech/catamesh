3b86ee81195b202b4484ae94b2699acb0f48faba
fabiangftech <fabian.gf.tech@gmail.com>
2026-03-17 15:06:36 -0300

    feature: add update resource add dependency

--- catamesh-core/src/main/java/dev/catamesh/application/handler/UpdateResourceHandler.java
+++ catamesh-core/src/main/java/dev/catamesh/application/handler/UpdateResourceHandler.java
@@ -1,9 +1,17 @@
 package dev.catamesh.application.handler;
 
+import dev.catamesh.core.cqrs.Command;
 import dev.catamesh.core.handler.ApplyDataProductContext;
 import dev.catamesh.core.handler.Handler;
+import dev.catamesh.core.model.Resource;
 
 public class UpdateResourceHandler<C> extends Handler<C> {
+    private final Command<Resource, Resource> updateResourceCommand;
+
+    public UpdateResourceHandler(Command<Resource, Resource> updateResourceCommand) {
+        this.updateResourceCommand = updateResourceCommand;
+    }
+
     @Override
     protected void doHandle(C context) {
         ApplyDataProductContext applyDataProductContext = (ApplyDataProductContext) context;
