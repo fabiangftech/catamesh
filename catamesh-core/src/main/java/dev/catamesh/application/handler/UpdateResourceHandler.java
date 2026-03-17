b10c614b4f66e3c717e3c0346c133c92d3e3f4a0
fabiangftech <fabian.gf.tech@gmail.com>
2026-03-17 17:33:53 -0300

    chore: add UpdateResourceHandler

--- catamesh-core/src/main/java/dev/catamesh/application/handler/UpdateResourceHandler.java
+++ catamesh-core/src/main/java/dev/catamesh/application/handler/UpdateResourceHandler.java
@@ -3,7 +3,7 @@ package dev.catamesh.application.handler;
 import dev.catamesh.core.cqrs.Command;
 import dev.catamesh.core.handler.ApplyDataProductContext;
 import dev.catamesh.core.handler.Handler;
-import dev.catamesh.core.model.Resource;
+import dev.catamesh.core.model.*;
 
 public class UpdateResourceHandler<C> extends Handler<C> {
     private final Command<Resource, Resource> updateResourceCommand;
@@ -15,6 +15,16 @@ public class UpdateResourceHandler<C> extends Handler<C> {
     @Override
     protected void doHandle(C context) {
         ApplyDataProductContext applyDataProductContext = (ApplyDataProductContext) context;
-        //todo impl
+        PlanResult planResult = applyDataProductContext.getPlanResult();
+        planResult.getSteps()
+                .stream()
+                .filter(step -> PlanStepType.RESOURCE.equals(step.getType()))
+                .filter(step -> PlanAction.UPDATE.equals(step.getAction()))
+                .forEach(step -> {
+                    Resource resource = ApplyDataProductContext.resolve(applyDataProductContext, step.getPath());
+                    resource.setDataProductId(Key.create(ApplyDataProductContext.resolveDataProductId(applyDataProductContext, resource)));
+                    updateResourceCommand.execute(resource);
+                    applyDataProductContext.markStepExecuted(step.getPath());
+                });
     }
 }
