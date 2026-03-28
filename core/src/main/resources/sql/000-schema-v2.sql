c528cac55806ab5bd2d7cc082c95b81913d1037e
fabiangftech <fabian.gf.tech@gmail.com>
2026-03-28 10:11:15 -0300

    chore: base schema-v2

---
+++ core/src/main/resources/sql/000-schema-v2.sql
@@ -0,0 +1,5 @@
+create table if not exists data_product();
+create table if not exists metadata();
+create table if not exists spec();
+create table if not exists resource();
+create table if not exists resource_definition();
